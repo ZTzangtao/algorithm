ca2ec47949947f756835a8e1d17da19a2b45d0ad
Tommy <zangtao3211@163.com>
2021-06-06 22:35:08 +0800

    算法初级02 原始快排

--- algorithmFromGodLeftFighting/src/primary01/Code_08_NetherlandsFlag.java
+++ algorithmFromGodLeftFighting/src/primary01/Code_08_NetherlandsFlag.java
@@ -1,10 +1,17 @@
 package primary01;
 
+import java.util.Arrays;
+
 /**
  * @Author: Tommy
  * @DATE: 2021/6/5
  */
 public class Code_08_NetherlandsFlag {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,4,3,2,7,5,3,8,23,4,8};
+        System.out.println(Arrays.toString(partition(arr,0, arr.length - 1,8)));
+    }
 
     public static int[] partition(int[] arr, int L, int R, int num) {
         int less = L - 1;
@@ -19,6 +26,7 @@ public class Code_08_NetherlandsFlag {
             }
         }
         return new int[]{less + 1, more - 1};
+//        return arr;
     }
 
     private static void swap(int[] arr, int i, int j) {
