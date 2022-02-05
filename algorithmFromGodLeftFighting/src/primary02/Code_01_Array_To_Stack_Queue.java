b4314a32691f84a674411df877beae5493779aa5
Tommy <zangtao3211@163.com>
2022-02-05 23:55:10 +0800

    算法初级03 队列问题

--- algorithmFromGodLeftFighting/src/primary02/Code_01_Array_To_Stack_Queue.java
+++ algorithmFromGodLeftFighting/src/primary02/Code_01_Array_To_Stack_Queue.java
@@ -43,7 +43,46 @@ public class Code_01_Array_To_Stack_Queue {
     }
 
     public static class ArrayQueue{
+        private Integer[] arr;
+        private Integer size;
+        private Integer start;
+        private Integer end;
 
+        public ArrayQueue(int initSize){
+            if (initSize < 0){
+                throw new IllegalArgumentException("The init size is less than 0");
+            }
+            arr = new Integer[initSize];
+            size = 0;
+            start = 0;
+            end = 0;
+        }
+
+        public Integer peek(){
+            if(size == 0){
+                return null;
+            }
+            return arr[start];
+        }
+
+        public void push(int obj){
+            if (size == arr.length){
+                throw new ArrayIndexOutOfBoundsException("The queue is full");
+            }
+            size++;
+            arr[end] = obj;
+            end = end == arr.length - 1 ? 0 : end + 1;
+        }
+
+        public Integer poll(){
+            if(size == 0){
+                throw new ArrayIndexOutOfBoundsException("The queue is empty");
+            }
+            size--;
+            int tmp = start;
+            start = start == arr.length - 1 ? 0 : start + 1;
+            return arr[tmp];
+        }
     }
 
 }
---
+++ algorithmFromGodLeftFighting/src/primary02/Code_03_StackAndQueueConvert.java
@@ -0,0 +1,99 @@
+package primary02;
+
+
+import org.omg.SendingContext.RunTime;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * @Author: Tommy
+ * @DATE: 2022/2/5
+ */
+public class Code_03_StackAndQueueConvert {
+
+    public static class TwoStackQueue{
+        private Stack<Integer> stackPush;
+        private Stack<Integer> stackPop;
+
+        public TwoStackQueue(int pushInt){
+          stackPush = new Stack<Integer>();
+          stackPop = new Stack<Integer>();
+        }
+
+        public void push(int pushInt){
+            stackPush.push(pushInt);
+        }
+
+        public int poll(){
+            if(stackPop.empty() && stackPush.empty()){
+                throw new RuntimeException("Queue is empty!");
+            } else if(stackPop.empty()){
+                while (!stackPush.empty()){
+                    stackPop.push(stackPush.pop());
+                }
+            }
+            return stackPop.pop();
+        }
+
+        public int peek(){
+            if(stackPop.empty() && stackPush.empty()){
+                throw new RuntimeException("Queue is empty!");
+            }else if(stackPop.empty()){
+                while (!stackPush.empty()){
+                    stackPop.push(stackPop.pop());
+                }
+            }
+            return stackPop.peek();
+        }
+
+    }
+
+
+    public static class TwoQueueStack {
+        private Queue<Integer> data;
+        private Queue<Integer> help;
+
+        public TwoQueueStack(){
+            data = new LinkedList<Integer>();
+            help = new LinkedList<Integer>();
+        }
+
+        public void push(int pushInt){
+            data.add(pushInt);
+        }
+
+        public int peek(){
+            if (data.isEmpty()){
+                throw new RuntimeException("Stack is empty!");
+            }
+
+            while (data.size() != 1){
+                help.add(data.poll());
+            }
+            int res = data.poll();
+            help.add(res);
+            swap();
+            return res;
+        }
+
+        public int pop(){
+            if (data.isEmpty()){
+                throw new RuntimeException("Stack is empty!");
+            }
+            while (data.size() > 1){
+                help.add(data.poll());
+            }
+            int res = data.poll();
+            swap();
+            return res;
+        }
+
+        private void swap() {
+            Queue<Integer> tmp = help;
+            help = data;
+            data = tmp;
+        }
+    }
+}
