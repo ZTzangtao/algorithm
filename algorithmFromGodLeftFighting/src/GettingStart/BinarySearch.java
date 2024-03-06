81c77fb5fc6e31737e6bf7a1546a51ec1026f230
Tommy <zangtao3211@163.com>
2024-03-06 21:29:18 +0800

    二分查找

--- algorithmFromGodLeftFighting/src/GettingStart/BinarySearch.java
+++ algorithmFromGodLeftFighting/src/GettingStart/BinarySearch.java
@@ -8,6 +8,42 @@ public class BinarySearch {
         System.out.println(maxIndex(new int[]{1,3,4,67,87,967,657465,27465745}, 9670));
     }
 
+    /**
+     * 找峰值点
+     * https://leetcode.cn/problems/find-peak-element/submissions/508516559/
+     * @param arr
+     * @return
+     */
+    public static int findPeakElement(int[] arr) {
+        int n = arr.length;
+        if (n == 1) {
+            return 0;
+        }
+        // 单独验证0位置
+        if (arr[0] > arr[1]) {
+            return 0;
+        }
+        // 单独验证 n - 1 位置
+        if (arr[n - 1] > arr[n - 2]) {
+            return n - 1;
+        }
+        int l = 1, r = n - 2,m = 0,ans = -1;
+        while (l <= r) {
+            m = l + ((r - l) >> 1);
+            if (arr[m - 1] > arr[m]) {
+                r = m - 1;
+            } else if (arr[m] < arr[m + 1]) {
+                l = m + 1;
+            } else {
+                ans = m;
+                break;
+            }
+
+        }
+        return ans;
+    }
+
+
     //大于等于num的最小位置
     public static int minIndex(int[] arr, int num) {
         if (arr.length == 0 || arr == null) {
