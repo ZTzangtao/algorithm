b96ebacc31dab65ec57cd21af7d0dfd5fcc1d850
Tommy <zangtao3211@163.com>
2024-03-06 20:07:57 +0800

    二分查找

--- algorithmFromGodLeftFighting/src/GettingStart/BinarySearch.java
+++ algorithmFromGodLeftFighting/src/GettingStart/BinarySearch.java
@@ -3,8 +3,51 @@ package GettingStart;
 public class BinarySearch {
 
     public static void main(String[] args) {
-        System.out.println(exist(new int[]{1,3,4,67,87,967,657465,27465745}, 967));
+//        System.out.println(exist(new int[]{1,3,4,67,87,967,657465,27465745}, 967));
+//        System.out.println(minIndex(new int[]{1,3,4,67,87,967,657465,27465745}, 9670));
+        System.out.println(maxIndex(new int[]{1,3,4,67,87,967,657465,27465745}, 9670));
     }
+
+    //大于等于num的最小位置
+    public static int minIndex(int[] arr, int num) {
+        if (arr.length == 0 || arr == null) {
+            return -1;
+        }
+        int ans = -1;
+        int l = 0, r = arr.length -1, m = 0;
+        while (l <= r) {
+            // 取中间值 (防止大于int的最大值)
+            m = l + ((r - l) >> 1);
+            if (arr[m] >= num) {
+                ans = m;
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return ans;
+    }
+
+    public static int maxIndex(int[] arr, int num) {
+        if (arr.length == 0 || arr == null) {
+            return -1;
+        }
+        int ans = -1;
+        int l = 0, r = arr.length -1, m = 0;
+        while (l <= r) {
+            // 取中间值 (防止大于int的最大值)
+            m = l + ((r - l) >> 1);
+            if (arr[m] > num) {
+
+                r = m - 1;
+            } else {
+                ans = m;
+                l = m + 1;
+            }
+        }
+        return ans;
+    }
+
 
     // arr 有序的情况下，判断num存在与否
     public static boolean exist(int[] arr, int num) {
