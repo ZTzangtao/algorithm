6338ffbb1143b53c94e5e09debee4be163bef762
zangtao <TZANG1@ford.com>
2024-03-10 22:42:28 +0800

    合并有序链表

--- algorithmFromGodLeftFighting/src/GettingStart/MergeTwoLists10.java
+++ algorithmFromGodLeftFighting/src/GettingStart/MergeTwoLists10.java
@@ -18,9 +18,16 @@ public class MergeTwoLists10 {
         ListNode cur2 = head == head1 ? head2 : head1;
         ListNode pre = head;
         while (cur1 != null && cur2 != null) {
-
+            if (cur1.val <= cur1.val) {
+                pre.next = cur1;
+                cur1 = cur1.next;
+            } else {
+                pre.next = cur2;
+                cur2 = cur2.next;
+            }
+            pre = pre.next;
         }
-
+        pre.next = cur1 != null ? cur1 : cur2;
 
         return head;
     }
