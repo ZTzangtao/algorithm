bcb90d2ed27629515f374981a3122713b3bcf3ae
Tommy <zangtao3211@163.com>
2024-03-14 22:15:54 +0800

    用队列实现栈

--- algorithmFromGodLeftFighting/src/primary02/Code_03_StackAndQueueConvert.java
+++ algorithmFromGodLeftFighting/src/primary02/Code_03_StackAndQueueConvert.java
@@ -6,22 +6,28 @@ import java.util.Queue;
 import java.util.Stack;
 
 /**
+ *
+ *
  * @Author: Tommy
  * @DATE: 2022/2/5
  */
 public class Code_03_StackAndQueueConvert {
 
     /**
+     * https://leetcode.cn/problems/implement-queue-using-stacks/submissions/512001285/
+     *
      * 两个栈实现队列
      *  1.pop只有空了，才能往pop里倒数据
      *  2.如果要倒数据，数据一定要清完
      *
      */
     public static class TwoStackQueue {
+        // in
         private Stack<Integer> stackPush;
+        //往外弹的
         private Stack<Integer> stackPop;
 
-        public TwoStackQueue(int pushInt){
+        public TwoStackQueue(){
           stackPush = new Stack<Integer>();
           stackPop = new Stack<Integer>();
         }
@@ -61,8 +67,8 @@ public class Code_03_StackAndQueueConvert {
         private Queue<Integer> help;
 
         public TwoQueueStack(){
-            data = new LinkedList<Integer>();
-            help = new LinkedList<Integer>();
+            data = new LinkedList<>();
+            help = new LinkedList<>();
         }
 
         public void push(int pushInt){
@@ -101,4 +107,34 @@ public class Code_03_StackAndQueueConvert {
             data = tmp;
         }
     }
+
+    public static class MyStack {
+        Queue<Integer> queue;
+
+        public MyStack() {
+            queue = new LinkedList<>();
+        }
+
+        public void push (int x) {
+            int n = queue.size();
+            queue.offer(x);
+            for (int i = 0; i < n; i++) {
+                queue.offer(queue.poll());
+            }
+        }
+
+        public int pop () {
+            return queue.poll();
+        }
+
+        public int top () {
+            return queue.peek();
+        }
+
+        public boolean empty() {
+            return queue.isEmpty();
+        }
+
+    }
+
 }
