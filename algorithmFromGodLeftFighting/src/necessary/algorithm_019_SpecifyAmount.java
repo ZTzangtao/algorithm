8dacf46fc2d969a542ffe794225dd466ca0d893d
Tommy <zangtao3211@163.com>
2024-03-23 15:11:16 +0800

    019 算法题常见风格

--- algorithmFromGodLeftFighting/src/necessary/algorithm_019_SpecifyAmount.java
+++ algorithmFromGodLeftFighting/src/necessary/algorithm_019_SpecifyAmount.java
@@ -34,8 +34,31 @@ public class algorithm_019_SpecifyAmount {
         out.close();
     }
 
-    private static boolean maxSumSubmatrix(int[][] mat, int n, int m) {
-        return false;
+    // 求子矩阵的最大累加和
+    private static int maxSumSubmatrix(int[][] mat, int n, int m) {
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++) {
+            // 需要辅助数组，临时动态生成
+            int[] arr = new int[m];
+            for (int j = i; j < n; j++) {
+                for(int k = 0; k < m; k++) {
+                    arr[k] += mat[j][k];
+                }
+                max = Math.max(max, maxSumSubarray(arr, m));
+            }
+        }
+        return max;
+    }
+
+    private static int maxSumSubarray(int[] arr, int m) {
+        int max = Integer.MIN_VALUE;
+        int cur = 0;
+        for (int i = 0; i < m; i++) {
+            cur += arr[i];
+            max = Math.max(max, cur);
+            cur = cur < 0 ? 0 : cur;
+        }
+        return max;
     }
 
 }
