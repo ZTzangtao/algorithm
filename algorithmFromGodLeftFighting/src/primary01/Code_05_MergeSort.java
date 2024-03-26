dd5a211ce93b5d80092e6eedfbcd36346b57a16b
Tommy <zangtao3211@163.com>
2024-03-26 23:05:42 +0800

    021 merge归并排序 非递归版本

--- algorithmFromGodLeftFighting/src/primary01/Code_05_MergeSort.java
+++ algorithmFromGodLeftFighting/src/primary01/Code_05_MergeSort.java
@@ -10,6 +10,52 @@ import java.util.Arrays;
  */
 public class Code_05_MergeSort {
 
+    public static final int[] arr = {};
+
+    /**
+     * 归并排序非递归版，时间复杂度O(n * logn)
+     * 空间复杂度O(n)
+     */
+    public static void mergeSort2(int n) {
+        // 一共发生O(logn)次
+        for(int l, m, r, step = 1; step < n; step <<=1 ) {
+            // 内部分组merge, 时间复杂度O(n)
+            l = 0;
+            while (l < n) {
+                m = l + step - 1;
+                if (m + 1 >= n) {
+                    // 已经没有右侧了
+                    break;
+                }
+                // 有右侧，求右侧的右边界
+                r = Math.min(l + (step << 1) - 1, n - 1);
+                merge(l, m, r);
+                l = r + 1;
+            }
+        }
+    }
+
+    public static void merge(int L, int mid, int R) {
+        int[] help = new int[R - L + 1];
+        int i = 0;
+        int p1 = L;
+        int p2 = mid + 1;
+        while (p1 <= mid && p2 <= R) {
+            help[i++] =
+                    arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        //两个必有且只有一个越界
+        while (p1 <= mid) {
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= R) {
+            help[i++] = arr[p2++];
+        }
+        for (i = 0; i < help.length; i++) {
+            arr[L + i] = help[i];
+        }
+    }
+
     public static void main(String[] args) {
         int[] n = new int[]{1,54,8,7,6,3,2,8,0,34,3,1,23,4,6,3};
         mergeSort(n);
