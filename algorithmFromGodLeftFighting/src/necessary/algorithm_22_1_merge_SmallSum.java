5b89071a91ea968e5e4c263cac4b29d1135698ea
Tommy <zangtao3211@163.com>
2024-03-28 22:34:41 +0800

    022 求所有小和问题，归并分支

---
+++ algorithmFromGodLeftFighting/src/necessary/algorithm_22_1_merge_SmallSum.java
@@ -0,0 +1,73 @@
+package necessary;
+
+import java.io.*;
+
+public class algorithm_22_1_merge_SmallSum {
+
+    public static int MAXN = 100001;
+
+    public static int[] arr = new int[MAXN];
+
+    public static int[] help = new int[MAXN];
+
+    public static int n;
+
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer in = new StreamTokenizer(br);
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+        while (in.nextToken() != StreamTokenizer.TT_EOF) {
+            n = (int) in.nval;
+            for (int i = 0; i < n; i++) {
+                in.nextToken();
+                arr[i] = (int) in.nval;
+            }
+            out.println(smallSum(0, n - 1));
+        }
+        out.flush();
+        out.close();
+    }
+
+    private static long smallSum(int l, int r) {
+        if (l == r) {
+            return 0;
+        }
+        int m = (l + r) / 2;
+        return smallSum(l, m) + smallSum(m+1, r) + merge(l, m, r);
+
+    }
+
+    // 返回跨左右产生的小和累加和, 左侧有序，右侧有序，让左右两侧整体有序
+    private static long merge(int l, int m, int r) {
+        // 统计部分
+        long ans = 0;
+        for (int j = m + 1, i = l, sum = 0; j <= r; j++) {
+            while (i <= m && arr[i] <= arr[j]) {
+                sum += arr[i++];
+            }
+            ans += sum;
+        }
+        // 正常merge
+        int i = l;
+        int p1 = l;
+        int p2 = m + 1;
+        while (p1 <= m && p2 <= r) {
+            // 从小到大赋值到help数组上
+            help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        //两个必有且只有一个越界
+        while (p1 <= m) {
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= r) {
+            help[i++] = arr[p2++];
+        }
+        for (i = 0; i < help.length; i++) {
+            arr[l + i] = help[i];
+        }
+        return ans;
+    }
+
+}
