77ed8fc93e12dd6caa07854789e1c716845ae645
Tommy <zangtao3211@163.com>
2024-04-10 22:39:45 +0800

    nqueue for exam

---
+++ algorithmFromGodLeftFighting/src/leetcode/NQueue.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NQueue {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> list = new ArrayList<>();
+        String point = ".";
+        String q = "Q";
+
+        return list;
+    }
+}
