b4346f4d3180b6f1d6637bd64cf64a672a05f0da
zangtao <TZANG1@ford.com>
2024-05-19 18:41:29 +0800

    fix

--- algorithmFromGodLeftFighting/src/primary01/HeapSort.java
+++ algorithmFromGodLeftFighting/src/primary01/HeapSort.java
@@ -26,10 +26,10 @@ public class HeapSort {
             heapInsert(arr, i);
         }
         int heapSize = arr.length;
-        swap(arr, 0, --heapSize);
-        while (heapSize > 0){
-            heapify(arr,0, heapSize);
+//        swap(arr, 0, --heapSize);
+        while (heapSize > 1) {
             swap(arr,0, --heapSize);
+            heapify(arr,0, heapSize);
         }
     }
 
@@ -39,11 +39,13 @@ public class HeapSort {
      * @param arr
      * @param index
      */
+    // arr[index] 这个数是新来的，往上看，与父亲节点比较，直到不比父亲大，或者来到顶
     public static void heapInsert(int[] arr, int index) {
         // 当前值与父亲节点相比，如果大于父亲节点就交换
         //i -> 父亲节点的值
         while (arr[index] > arr[(index - 1) / 2]) {
             swap(arr, index, (index - 1)/2);
+            // 来到父亲节点的位置
             index = (index - 1) / 2;
         }
     }
@@ -54,19 +56,30 @@ public class HeapSort {
      * @param index 位置
      * @param heapSize 堆的最右边
      */
+    // 从i位置变， i位置的数变小了，又想维持大根堆结构
     public static void heapify(int[] arr, int index, int heapSize){
-        // 左孩子
+        // 左孩子下标
         int left = index * 2 + 1;
+
+        // 有左孩子
         while (left < heapSize) {
-            int largest = left + 1 < heapSize && arr[left + 1] > arr[left] ?
-                    left + 1 : left;
+            // 右孩子下标
+            int right = left + 1;
+
+            // 评选哪个更大
+            // 两个条件都满足才是右孩子，不然都是左孩子最大
+            int largest = right < heapSize && arr[right] > arr[left] ?
+                    right : left;
+            // 与自己比较，谁是最大的下标
             largest = arr[largest] > arr[index] ? largest : index;
-            if (largest == index){
+            if (largest == index) {
+                // 最强的是自己
                 break;
             }
             // largest != index
-            swap(arr, largest,index);
+            swap(arr, largest, index);
             index = largest;
+            // 左孩子下标
             left = index * 2 + 1;
 
         }
