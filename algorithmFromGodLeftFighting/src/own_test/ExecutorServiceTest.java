1bcff818d13fd0a21ad9b431ee4c8058f2633c8a
Tommy <zangtao3211@163.com>
2024-06-05 00:07:17 +0800

    多线程基础相关

--- algorithmFromGodLeftFighting/src/own_test/ExecutorServiceTest.java
+++ algorithmFromGodLeftFighting/src/own_test/ExecutorServiceTest.java
@@ -6,6 +6,7 @@ import java.util.concurrent.Executors;
 public class ExecutorServiceTest implements Runnable {
 
     public static void main(String[] args) {
+        // 固定线程池 无界阻塞队列 -> arrayBlockingQueue
         ExecutorService executorService = Executors.newFixedThreadPool(10);
         executorService.execute(new ExecutorServiceTest());
     }
