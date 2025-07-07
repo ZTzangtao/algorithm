d59e4567c0b5d145a80808be6015615fbef89f75
Tommy <zangtao3211@163.com>
2025-07-08 00:39:33 +0800

    thread basic join

--- algorithmFromGodLeftFighting/src/multiThread/basics/MiTestJoin.java
+++ algorithmFromGodLeftFighting/src/multiThread/basics/MiTestJoin.java
@@ -3,17 +3,21 @@ package multiThread.basics;
 public class MiTestJoin {
 
     /**
-     * 可以通过Thread的静态方法yield，让当前现成从运行状态转变为就绪状态
+     * 在main线程中调用了t1.join(),那么main线程会进入到等待状态，需要等待t1线程全部执行完毕，再恢复到就绪状态等待CPU调度。
+     * 在main线程中调用了t1.join(2000),那么main线程会进入到等待状态，需要等待t1执行2秒后，再恢复到就绪状态等待CPU调度。
+     * 如果在等待期间，t1已经结束了
      *
      * @param args
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread t1 = new Thread(() -> {
             for (int i = 0; i < 100; i++) {
-                if (i == 50) {
-                    Thread.yield();
-                }
                 System.out.println("t1:" + i);
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
         });
 
@@ -24,6 +28,15 @@ public class MiTestJoin {
         });
         t2.start();
         t1.start();
+        for (int i = 0; i < 10; i++) {
+            System.out.println("main" + i);
+            Thread.sleep(1000);
+            if (i == 1) {
+                t1.join(2000);
+            }
+        }
+
+
     }
 
 }
